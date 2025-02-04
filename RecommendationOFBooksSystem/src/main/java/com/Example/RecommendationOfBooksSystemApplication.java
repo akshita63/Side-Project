d68babe0118538a982feb87a104bdc33000fb62d
Akshita <302000akshita@gmail.com>
2025-02-04 18:46:23 +0530

    changes

---
+++ RecommendationOFBooksSystem/src/main/java/com/Example/RecommendationOfBooksSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationOfBooksSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationOfBooksSystemApplication.class, args);
+	}
+
+}
---
+++ RecommendationOFBooksSystem/src/test/java/com/Example/RecommendationOfBooksSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationOfBooksSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
